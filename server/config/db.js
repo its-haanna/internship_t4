c94518159ce89b77954ebee1930f3d2ab5ce32c6
Ha Anna <its.haanna@gmail.com>
2022-09-29 02:22:27 +0900

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'itransitionDB'
+  host: 'eu-cdbr-west-03.cleardb.net',
+  user: 'b319d74bcd1c38',
+  password: 'ff81dbab',
+  database: 'heroku_2ca833d14744c14'
 });
 
 module.exports = db;
